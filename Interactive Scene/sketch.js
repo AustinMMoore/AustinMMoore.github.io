8758d4b2e462b94e4ad06ca2d3d151e4c6296bd3
Austin Moore <pillowtree2@gmail.com>
2019-02-14 15:20:09 -0600

    Added code from p5js online

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -1,4 +1,4 @@
-// Project Title
+// Super Square!
 // Your Name
 // Date
 //
@@ -7,9 +7,92 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+	background(255)
+	//noStroke()
+	xPosition = width/2;
+	yPosition = height/2;
+  rectWidth = random(10, 50);
+	rectHeight = random(10, 50);
+  rectColourR = 255;
+  rectColourG = 255;
+  rectColourB = 255;
 }
 
+let rectColourR;
+let rectColourG;
+let rectColourB;
+let xPosition;
+let yPosition;
+let xVelocity = 2;
+let yVelocity = 2;
+let rectWidth = 15;
+let rectHeight = 15;
+let rectRainbow = false
+
 function draw() {
-  background(220);
-  ellipse(mouseX, mouseY, 25, 25)
+  //background(220);
+	fill(rectColourR, rectColourG, rectColourB)
+	//fill(0, 255, 255);
+	rect(xPosition, yPosition, rectWidth, rectHeight)
+	
+	if (xPosition >= width - rectWidth || xPosition <= 0) {
+		xVelocity *= -1;
+	}
+		
+	if (yPosition >= height - rectHeight || yPosition <= 0) {
+		yVelocity *= -1;
+	}
+  if (rectRainbow === true) {
+    rectColourR = random(0, 255);
+    rectColourG = random(0, 255);
+    rectColourB = random(0, 255);
+  }
+  
+	xPosition += xVelocity;
+	yPosition += yVelocity;
+
+}
+
+function keyPressed() {
+
+  if (key === "r" || key === "R") {
+    rectColourR = 255;
+    rectColourG = 0;
+    rectColourB = 0;
+    rectRainbow = false;
+  }
+  else if (key === "g" || key === "G") {
+    rectColourR = 0;
+    rectColourG = 255;
+    rectColourB = 0;
+    rectRainbow = false;
+  }
+  else if (key === "b" || key === "B") {
+    rectColourR = 0;
+    rectColourG = 0;
+    rectColourB = 255;
+    rectRainbow = false;
+  }
+  else if (key === " ") {
+    rectRainbow = true;
+  }
+  else if (keyCode === LEFT_ARROW){
+    yVelocity *= 0.9
+  }
+  else if (keyCode === RIGHT_ARROW){
+    yVelocity *= 1.1
+  }
+  else if (keyCode === UP_ARROW){
+    xVelocity *= 1.1
+  }
+  else if (keyCode === DOWN_ARROW){
+    xVelocity *= 0.9
+  }
+}
+
+function mouseWheel(event) {
+  rectColourR = random(0, 255);
+  rectColourG = random(0, 255);
+  rectColourB = random(0, 255);
+  rectRainbow = false
 }
