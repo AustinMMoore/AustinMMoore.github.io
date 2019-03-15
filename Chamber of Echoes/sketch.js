77c7160364c20255131a9e0766f66bcaa618c571
AustinMMoore <pillowtree2@gmail.com>
2019-03-14 19:40:35 -0600

    buttons class integrated successfully

--- Chamber of Echoes/sketch.js
+++ Chamber of Echoes/sketch.js
@@ -9,32 +9,39 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
-  playButton = new Button(width/2, width/2, 300, 200, "PLAY");
+  textAlign(CENTER)
+  playButton = new Button(width/2, height/4, 300, 200, "PLAY", 40);
+  optionsButton = new Button(width/2, height/2, 250, 150, "OPTIONS", 30)
+  card1 = new Card(100, 100);
+  card2 = new Card(250, 100);
+  card3 = new Card(400, 100);
+  card4 = new Card(550, 100);
+  card5 = new Card(700, 100);
+  card6 = new Card(850, 100);
+  card7 = new Card(1000, 100);
 }
 
-let cardXPosition = 10;
-let cardYPosition = 10;
 let cardWidth = 100;
 let cardHeight = 160;
 let gameState = "menu";
-let playButtonHeight = 200;
-let playButtonWidth = 300;
-let buttonColour;
+let buttonTextSize;
 let cardScalar = 1;
 let card1, card2, card3, card4, card5, card6, card7;
-let playButton;
+let playButton, optionsButton;
 
 function draw() {
-  background(220);
+  background(240);
   if (gameState === "menu") {
-    // showPlayButton();
     playButton.show();
-    if (playButton.checkIfclicked()) {
+    optionsButton.show();
+    if (playButton.isClicked()) {
       gameState = "game";
+    }
+    if (optionsButton.isClicked()) {
+      gameState = "options";
     }
   }
   if (gameState === "game") {
-
     card1.behavior();
     card2.behavior();
     card3.behavior();
@@ -42,66 +49,11 @@ function draw() {
     card5.behavior();
     card6.behavior();
     card7.behavior();
-
-    // showCard();
-    // cardMovement();
-    // zoomOnCard();
+  }
+  if (gameState === "options") {
+    throw
   }
 }
-
-// function cardMovement() {
-//   if (mouseX >= cardXPosition && 
-//       mouseX <= cardXPosition + cardWidth &&
-//       mouseY >= cardYPosition && 
-//       mouseY <= cardYPosition + cardHeight & mouseIsPressed) {
-//     cardXPosition = mouseX - cardWidth / 2;
-//     cardYPosition = mouseY - cardHeight / 2;
-//     cardDragging = true;
-//   }
-// }
-
-// function showCard() {
-//   fill(100);
-//   rect(cardXPosition, cardYPosition, cardWidth * cardScalar, cardHeight * cardScalar);
-// }
-
-// function showPlayButton() {
-//   if (mouseX <= width / 2 + playButtonWidth/2 && 
-//       mouseX >= width / 2 - playButtonWidth/2 && 
-//       mouseY >= height / 2 - playButtonHeight / 2 - 100 && 
-//       mouseY <= height / 2 + playButtonHeight / 2 - 100 ) {
-//     buttonColour = 200;
-//   }
-//   else {
-//     buttonColour = 255;
-//   }
-//   if (gameState === "menu") {
-//     fill(buttonColour);
-//     rect(width/2, height/2 - 100, playButtonWidth, playButtonHeight);
-//   }
-// }
-
-// function mouseClicked() {
-//   if (mouseX <= width/2 + playButtonWidth/2 && 
-//       mouseX >= width/2 - playButtonWidth/2 && 
-//       mouseY >= height/2 - playButtonHeight/2 - 100 && 
-//       mouseY <= height/2 + playButtonHeight/2 - 100 ) {
-//     gameState = "game";
-//   }
-// }
-
-// function zoomOnCard() {
-//   if (mouseX >= cardXPosition && 
-//     mouseX <= cardXPosition + cardWidth &&
-//     mouseY >= cardYPosition && 
-//     mouseY <= cardYPosition + cardHeight &&
-//     !mouseIsPressed ) {
-//     cardScalar = 3;
-//   }
-//   else {
-//     cardScalar = 1;
-//   }
-// }
 
 class Card {
   constructor(x, y) {
@@ -131,11 +83,7 @@ class Card {
   }
 
   moveCard() {
-    if (mouseX >= this.x - this.width/2 && 
-        mouseX <= this.x + this.width/2 &&
-        mouseY >= this.y - this.height/2 && 
-        mouseY <= this.y + this.height/2 && 
-        mouseIsPressed) {
+    if (mouseX >= this.x - this.width/2 && mouseX <= this.x + this.width/2 &&mouseY >= this.y - this.height/2 && mouseY <= this.y + this.height/2 && mouseIsPressed) {
       this.x = mouseX;
       this.y = mouseY;
     }
@@ -148,40 +96,35 @@ class Card {
   }
 }
 
-card1 = new Card(100, 100);
-card2 = new Card(250, 100);
-card3 = new Card(400, 100);
-card4 = new Card(550, 100);
-card5 = new Card(700, 100);
-card6 = new Card(850, 100);
-card7 = new Card(1000, 100);
-
 class Button {
-  constructor(x, y, width, height, text) {
+  constructor(x, y, width, height, text, textSize) {
     this.height = height;
     this.width = width;
     this.x = x;
     this.y = y;
     this.buttonText = text;
+    this.buttonTextSize = textSize
+  }
+
+  isSelected() {
+    return mouseX >= this.x - this.width/2 && mouseX <= this.x + this.width/2 &&mouseY >= this.y - this.height/2 && mouseY <= this.y + this.height/2;
   }
 
   show() {
-    fill(255);
-    rect(this.x, this.y, this.width, this.height);
-    text(this.buttonText, this.x, this.y);
-    if (this.checkIfSelected) {
-      fill(0);
+    if (this.isSelected()) {
+      fill(200);
     }
+    else {
+      fill(255);
+    }
+    rect(this.x, this.y, this.width, this.height);
+    fill(0)
+    textSize(this.buttonTextSize);
+    text(this.buttonText, this.x, this.y + this.buttonTextSize/2);
+    return this.buttonText;
   }
 
-  checkIfSelected() {
-    return mouseX >= this.x - this.width/2 && 
-           mouseX <= this.x + this.width/2 &&
-           mouseY >= this.y - this.height/2 && 
-           mouseY <= this.y + this.height/2;
-  }
-
-  checkIfclicked() {
-    return this.checkIfSelected() && mouseIsPressed;
+  isClicked() {
+    return this.isSelected() && mouseIsPressed;
   }
 }
